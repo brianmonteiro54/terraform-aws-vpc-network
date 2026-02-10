413b1e538337362c8b3c9b328cecc40754b2e6fa
Brian Richard <brian.monteiro@escoladanuvem.org>
2026-02-10 02:19:15 -0300

    fix: update outputs to match dynamic resource names with count

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,5 +1,5 @@
 output "vpc_id" {
-  value = aws_vpc.terraform_vpc.id
+  value       = aws_vpc.terraform_vpc.id
   description = "VPC ID"
 }
 
@@ -24,21 +24,21 @@ output "private_subnet_ids" {
 }
 
 output "public_route_table_id" {
-  value       = aws_route_table.public_route_table.id
+  value       = aws_route_table.public_rt.id
   description = "Public route table ID"
 }
 
 output "private_route_table_ids" {
-  value       = [aws_route_table.private_route_table_1.id, aws_route_table.private_route_table_2.id]
+  value       = aws_route_table.private_rts[*].id
   description = "Private route table IDs"
 }
 
 output "nat_gateway_ids" {
-  value       = [aws_nat_gateway.nat_gateway_1.id, aws_nat_gateway.nat_gateway_2.id]
+  value       = aws_nat_gateway.nat_gateways[*].id
   description = "NAT Gateway IDs"
 }
 
 output "nat_eip_ids" {
-  value       = [aws_eip.nat_eip_1.id, aws_eip.nat_eip_2.id]
+  value       = aws_eip.nat_eips[*].id
   description = "EIP allocation IDs used by NAT Gateways"
 }
